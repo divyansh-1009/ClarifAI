39acba3a93f8d62e16c1656dc3e5b672f387836c
Shaurya Dwivedi <shauryadwivedi2001@gmail.com>
2026-04-09 14:39:15 +0530

    added req for resend otp mail

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
     'topic',
     'knowledge',
     'query',
+    'anymail',
 ]
 
 MIDDLEWARE = [
@@ -179,11 +180,9 @@ SIMPLE_JWT = {
 # Email Configuration
 EMAIL_BACKEND = os.getenv(
     'EMAIL_BACKEND',
-    'django.core.mail.backends.console.EmailBackend',
+    'anymail.backends.resend.EmailBackend',
 )
-EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL') or EMAIL_HOST_USER
+ANYMAIL = {
+    "RESEND_API_KEY": os.getenv("EMAIL_HOST_PASSWORD")
+}
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'onboarding@resend.dev')
